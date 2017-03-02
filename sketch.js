53c6a817dccb55a99ae4f1f618ae7915078f5a21
yukakosunaba <y.sunaba1@arts.ac.uk>
2017-03-02 14:10:21 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var spot={
+  x:100,
+  y:50
+};
+
+var col= {
+  r:255,
+  g:0,
+  b:0
+};
+
+
+
+function setup() {
+  createCanvas(600,400);
+  background(0);
+}
+
+function draw() {
+  col.r=random(100,255);
+  col.g=random(100,255);
+  col.b=random(100,255);
+  spot.x=random(0,width);
+  spot.y=random(0,height);
+  noStroke()
+  fill(col.r,col.g,col.b);
+  ellipse(spot.x,spot.y,24,24);
+  //ellipse(point.x,point.y,24,24);
+}
